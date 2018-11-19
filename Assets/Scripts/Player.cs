04f62f7c879987c5cd2dd89e4a3a63a0405e45dd
Jerry Zhao <sjs.zjy@gmail.com>
2018-11-19 22:35:35 +0800

    first Unity

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	Rigidbody2D playerRigid;
+	bool canJump = true;
+	float jumpVel = 0;
+	// Use this for initialization
+	void Start () {
+		playerRigid = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// float speed = Input.GetAxisRaw("Horizontal")*Time.deltaTime;
+		// playerRigid.transform.Translate(speed,0,0);
+		RaycastHit2D hitGround;
+		// hitGround;
+		if(Input.GetKey(KeyCode.Space) && canJump == true){
+			jumpVel = Input.GetAxis("Vertical");
+			Debug.Log("JumpVel is "+jumpVel);
+		}
+		playerRigid.velocity = new Vector2(Input.GetAxisRaw("Horizontal"),playerRigid.velocity.y + jumpVel);
+	}
+}
