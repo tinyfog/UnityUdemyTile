3867fcece6b2b3aa8ddb1c657a2b692ef9f5ab1b
Jerry Zhao <sjs.zjy@gmail.com>
2018-11-21 23:00:15 +0800

    modify the jump action to prefect

--- Assets/Scripts/Jerryzhao.cs
+++ Assets/Scripts/Jerryzhao.cs
@@ -6,9 +6,12 @@ public class Jerryzhao : MonoBehaviour
 {
     Rigidbody2D playerRigid;
     public Vector3 offset;
+    public float speed;
     public bool canJump = true;
     public float jumpForce = 5;
     Animator _anim;
+
+    bool readyJump = false;
     // Use this for initialization
     void Start()
     {
@@ -21,14 +24,14 @@ public class Jerryzhao : MonoBehaviour
     {
 
         Debug.DrawRay(transform.position + offset, Vector3.down * 1.1f, Color.red);
-		Move();
-		Jump();
+        Move();
+        JumpAnim();
     }
 
-    void Jump()
+    void JumpAnim()
     {
         RaycastHit2D hitGround = Physics2D.Raycast(transform.position + offset, Vector2.down, 1.1f, 1 << 8);
-   
+
         if (hitGround.collider != null)
         {
             Debug.Log("hitGround is " + hitGround.collider.gameObject.name);
@@ -37,14 +40,30 @@ public class Jerryzhao : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && canJump == true)
         {
-            playerRigid.velocity = new Vector2(playerRigid.velocity.x, jumpForce);
-            Debug.Log("jumpForce is " + jumpForce);
-            canJump = false;
+            _anim.Play("animJump");
+
         }
     }
-	void Move(){
-		float move = Input.GetAxisRaw("Horizontal");
-		_anim.SetFloat("move",move);
-		playerRigid.velocity = new Vector2(move, playerRigid.velocity.y);
-	}
+
+    void JumpAction()
+    {
+        playerRigid.velocity = new Vector2(playerRigid.velocity.x, jumpForce);
+        Debug.Log("jumpForce is " + jumpForce);
+        canJump = false;
+    }
+    void ReadyJumpBegin(){
+        readyJump = true;
+    }
+    void ReadyJumpEnd(){
+        readyJump = false;
+    }
+    void Move()
+    {
+        if(!readyJump){
+        float move = Input.GetAxisRaw("Horizontal") * speed;
+        _anim.SetFloat("move", move);
+        playerRigid.velocity = new Vector2(move, playerRigid.velocity.y);
+        }
+
+    }
 }
